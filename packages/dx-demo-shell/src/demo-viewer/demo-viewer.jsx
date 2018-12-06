0ea2e28299b9cf08bbe920640031708e7325070b
Sergey Alexeev <SergeyAlexeev@users.noreply.github.com>
2018-12-06 11:04:00 +0300

    Fix after merge

--- packages/dx-demo-shell/src/demo-viewer/demo-viewer.jsx
+++ packages/dx-demo-shell/src/demo-viewer/demo-viewer.jsx
@@ -8,15 +8,14 @@ import {
 import { ThemeViewer } from './theme-viewer';
 import { DemoFrame } from './demo-frame';
 import { SourceCode } from './source-code';
+import { EmbeddedDemoContext } from '../context';
 
 export class DemoViewer extends React.Component {
-  constructor(props, context) {
-    super(props, context);
-
-    const { embeddedDemoOptions: { defaultTab } } = context;
+  constructor(props) {
+    super(props);
 
     this.state = {
-      activeTab: defaultTab || 'preview',
+      activeTab: 'preview',
     };
 
     this.toggle = this.toggle.bind(this);
@@ -33,78 +32,81 @@ export class DemoViewer extends React.Component {
 
   render() {
     const { match: { params: { demoName, sectionName }, url } } = this.props;
-    const { embeddedDemoOptions: { showThemeSelector, demoSources } } = this.context;
     const { activeTab } = this.state;
 
     return (
-      <Switch>
-        <Route
-          path={`${url}/:themeName/:variantName/clean`}
-          render={({ match: { params: { themeName, variantName } } }) => (
-            <div>
-              <DemoFrame
-                themeName={themeName}
-                variantName={variantName}
-                sectionName={sectionName}
-                demoName={demoName}
-              />
-            </div>
-          )}
-        />
-        <Route
-          path={url}
-          render={() => (
-            <div style={{ paddingTop: '8px' }}>
-              <ThemeViewer
-                availableThemes={Object.keys(demoSources[sectionName][demoName])}
-              >
-                {({ themeName, variantName }) => (
-                  <div style={{ marginTop: showThemeSelector ? '-42px' : 0 }}>
-                    <Nav tabs>
-                      <NavItem>
-                        <NavLink
-                          className={activeTab === 'preview' ? 'active' : ''}
-                          onClick={() => { this.toggle('preview'); }}
+      <EmbeddedDemoContext.Consumer>
+        {({ showThemeSelector, demoSources }) => (
+          <Switch>
+            <Route
+              path={`${url}/:themeName/:variantName/clean`}
+              render={({ match: { params: { themeName, variantName } } }) => (
+                <div>
+                  <DemoFrame
+                    themeName={themeName}
+                    variantName={variantName}
+                    sectionName={sectionName}
+                    demoName={demoName}
+                  />
+                </div>
+              )}
+            />
+            <Route
+              path={url}
+              render={() => (
+                <div style={{ paddingTop: '8px' }}>
+                  <ThemeViewer
+                    availableThemes={Object.keys(demoSources[sectionName][demoName])}
+                  >
+                    {({ themeName, variantName }) => (
+                      <div style={{ marginTop: showThemeSelector ? '-42px' : 0 }}>
+                        <Nav tabs>
+                          <NavItem>
+                            <NavLink
+                              className={activeTab === 'preview' ? 'active' : ''}
+                              onClick={() => { this.toggle('preview'); }}
+                            >
+                              Preview
+                            </NavLink>
+                          </NavItem>
+                          <NavItem>
+                            <NavLink
+                              className={activeTab === 'source' ? 'active' : ''}
+                              onClick={() => { this.toggle('source'); }}
+                            >
+                              Source
+                            </NavLink>
+                          </NavItem>
+                        </Nav>
+                        <TabContent
+                          activeTab={activeTab}
+                          style={{ marginTop: '20px' }}
                         >
-                          Preview
-                        </NavLink>
-                      </NavItem>
-                      <NavItem>
-                        <NavLink
-                          className={activeTab === 'source' ? 'active' : ''}
-                          onClick={() => { this.toggle('source'); }}
-                        >
-                          Source
-                        </NavLink>
-                      </NavItem>
-                    </Nav>
-                    <TabContent
-                      activeTab={activeTab}
-                      style={{ marginTop: '20px' }}
-                    >
-                      <TabPane tabId="preview">
-                        <DemoFrame
-                          themeName={themeName}
-                          variantName={variantName}
-                          sectionName={sectionName}
-                          demoName={demoName}
-                        />
-                      </TabPane>
-                      <TabPane tabId="source">
-                        <SourceCode
-                          themeName={themeName}
-                          sectionName={sectionName}
-                          demoName={demoName}
-                        />
-                      </TabPane>
-                    </TabContent>
-                  </div>
-                )}
-              </ThemeViewer>
-            </div>
-          )}
-        />
-      </Switch>
+                          <TabPane tabId="preview">
+                            <DemoFrame
+                              themeName={themeName}
+                              variantName={variantName}
+                              sectionName={sectionName}
+                              demoName={demoName}
+                            />
+                          </TabPane>
+                          <TabPane tabId="source">
+                            <SourceCode
+                              themeName={themeName}
+                              sectionName={sectionName}
+                              demoName={demoName}
+                            />
+                          </TabPane>
+                        </TabContent>
+                      </div>
+                    )}
+                  </ThemeViewer>
+                </div>
+              )}
+            />
+          </Switch>
+        )}
+      </EmbeddedDemoContext.Consumer>
     );
   }
 }
@@ -117,8 +119,4 @@ DemoViewer.propTypes = {
     }),
     url: PropTypes.string.isRequired,
   }).isRequired,
-};
-
-DemoViewer.contextTypes = {
-  embeddedDemoOptions: PropTypes.object.isRequired,
 };
--- packages/dx-demo-shell/src/demo-viewer/theme-selector.jsx
+++ packages/dx-demo-shell/src/demo-viewer/theme-selector.jsx
@@ -3,89 +3,89 @@ import * as PropTypes from 'prop-types';
 import {
   UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem,
 } from 'reactstrap';
-
+import { EmbeddedDemoContext } from '../context';
 import './theme-selector.css';
 
-export const ThemeSelector = (
-  {
-    selectedThemeName, selectedVariantName, availableThemes, onChange,
-  },
-  { embeddedDemoOptions: { showThemeVariants, themeSources } },
-) => {
-  const selectedTheme = themeSources.find(({ name }) => name === selectedThemeName);
-
-  return (
-    <UncontrolledDropdown
-      className="theme-selector"
-    >
-      <DropdownToggle
-        tag="span"
-        caret
-      >
-        <a
-          href=""
-          onClick={(e) => {
-            e.preventDefault();
-          }}
+export const ThemeSelector = ({
+  selectedThemeName,
+  selectedVariantName,
+  availableThemes,
+  onChange,
+}) => (
+  <EmbeddedDemoContext.Consumer>
+    {({ showThemeVariants, themeSources }) => {
+      const selectedTheme = themeSources.find(({ name }) => name === selectedThemeName);
+      return (
+        <UncontrolledDropdown
+          className="theme-selector"
         >
-          {showThemeVariants
-            ? selectedTheme.variants.find(({ name }) => name === selectedVariantName).title
-            : selectedTheme.title}
-        </a>
-      </DropdownToggle>
-      <DropdownMenu right>
-        {themeSources.reduce((acc, { name: themeName, title: themeTitle, variants }) => {
-          const available = availableThemes.indexOf(themeName) > -1;
-          const activeTheme = themeName === selectedThemeName;
+          <DropdownToggle
+            tag="span"
+            caret
+          >
+            <a
+              href=""
+              onClick={(e) => {
+                e.preventDefault();
+              }}
+            >
+              {showThemeVariants
+                ? selectedTheme.variants.find(({ name }) => name === selectedVariantName).title
+                : selectedTheme.title}
+            </a>
+          </DropdownToggle>
+          <DropdownMenu right>
+            {themeSources.reduce((acc, { name: themeName, title: themeTitle, variants }) => {
+              const available = availableThemes.indexOf(themeName) > -1;
+              const activeTheme = themeName === selectedThemeName;
 
-          if (!available) return acc;
-          if (!showThemeVariants) {
-            acc.push(
-              <DropdownItem
-                key={themeName}
-                active={activeTheme}
-                onClick={() => {
-                  if (selectedThemeName !== themeName) {
-                    onChange(themeName, variants[0].name);
-                  }
-                }}
-              >
-                {themeTitle}
-              </DropdownItem>,
-            );
-          } else {
-            acc.push(variants.map(({ name: variantName, title: variantTitle }) => {
-              const activeVariant = variantName === selectedVariantName;
+              if (!available) return acc;
+              if (!showThemeVariants) {
+                acc.push(
+                  <DropdownItem
+                    key={themeName}
+                    active={activeTheme}
+                    onClick={() => {
+                      if (selectedThemeName !== themeName) {
+                        onChange(themeName, variants[0].name);
+                      }
+                    }}
+                  >
+                    {themeTitle}
+                  </DropdownItem>,
+                );
+              } else {
+                acc.push(variants.map(({ name: variantName, title: variantTitle }) => {
+                  const activeVariant = variantName === selectedVariantName;
 
-              return (
-                <DropdownItem
-                  key={`${themeName}|${variantName}`}
-                  active={activeTheme && activeVariant}
-                  onClick={() => {
-                    if (selectedThemeName !== themeName || selectedVariantName !== variantName) {
-                      onChange(themeName, variantName);
-                    }
-                  }}
-                >
-                  {variantTitle}
-                </DropdownItem>
-              );
-            }));
-          }
-          return acc;
-        }, [])}
-      </DropdownMenu>
-    </UncontrolledDropdown>
-  );
-};
+                  return (
+                    <DropdownItem
+                      key={`${themeName}|${variantName}`}
+                      active={activeTheme && activeVariant}
+                      onClick={() => {
+                        if (selectedThemeName !== themeName
+                          || selectedVariantName !== variantName) {
+                          onChange(themeName, variantName);
+                        }
+                      }}
+                    >
+                      {variantTitle}
+                    </DropdownItem>
+                  );
+                }));
+              }
+              return acc;
+            }, [])}
+          </DropdownMenu>
+        </UncontrolledDropdown>
+      );
+    }}
+  </EmbeddedDemoContext.Consumer>
+);
 
 ThemeSelector.propTypes = {
   selectedThemeName: PropTypes.string.isRequired,
   selectedVariantName: PropTypes.string.isRequired,
   availableThemes: PropTypes.arrayOf(PropTypes.string).isRequired,
   onChange: PropTypes.func.isRequired,
-};
-
-ThemeSelector.contextTypes = {
-  embeddedDemoOptions: PropTypes.object.isRequired,
 };
